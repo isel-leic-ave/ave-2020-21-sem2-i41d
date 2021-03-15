4d8842dd0fea9ef68da810db1644d06e5a1c5a36
Miguel Gamboa <miguelgamboa@outlook.com>
2021-03-15 12:43:39 +0000

    aulas 1 e 2

---
+++ aula01-jvm-interop/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void demo1() {
+        Point p = new Point(3, 7);
+        System.out.println("Module = " + p.getModule());
+    }
+
+    public static void main(String [] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ aula01-jvm-interop/Point.kt
@@ -0,0 +1,4 @@
+
+class Point(val x: Double, val y:Double) {
+    fun getModule() = Math.sqrt(x*x + y*y)
+}
