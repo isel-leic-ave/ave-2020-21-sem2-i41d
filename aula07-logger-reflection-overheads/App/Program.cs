18261f8abd9fedf596623a8ece013f15204b1587
Miguel Gamboa <miguelgamboa@outlook.com>
2021-04-06 17:11:51 +0100

    Setup aula 7

---
+++ aula07-logger-reflection-overheads/App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Logger;
+
+namespace App
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Point p = new Point(7, 9);
+            Student s1 = new Student(154134, "Ze Manel", 5243, "ze");
+            Student s2 = new Student(154134, "Xico", 1234, "xico");
+            Account a = new Account(1300);
+            // Console.WriteLine(p);
+            // Console.WriteLine(s1);
+            // Console.WriteLine(a);        
+            Log l = new Log();
+            l.Info(p);
+            l.Info(s1);
+            l.Info(s2);
+            l.Info(a);
+            
+        }
+    }
+}
